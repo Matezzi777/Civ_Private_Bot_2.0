5d12cf99ea40c22b4c7d329846f63451436c8d05
Matezzi777 <matezzi777@gmail.com>
2024-02-29 02:31:23 +0100

    $ping added

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
 
+    @bot.command()
+    async def ping(ctx : commands.Context):
+        embed = discord.Embed(colour=discord.Colour.green(), title="PONG")
+        await ctx.send(embed=embed)
+
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
