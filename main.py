2619711a5bc0b28c40fe8831cf4bf4f27626d557
Matezzi777 <matezzi777@gmail.com>
2024-03-13 01:04:34 +0100

    cogs etc. removed

--- main.py
+++ main.py
@@ -15,29 +15,10 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
 
-        for cmd_file in settings.COMMANDES_DIR.glob("*.py"):
-            if cmd_file.name != "__init__.py":
-                await bot.load_extension(f"commandes.{cmd_file.name[:-3]}")
-        for cog_file in settings.COGS_DIR.glob("*.py"):
-            if cog_file != "__init__.py":
-                await bot.load_extension(f"cogs.{cog_file.name[:-3]}")
-
-    @bot.event
-    async def on_command_error(ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Missing argument")
-
     @bot.command()
-    async def load(ctx : commands.context, cog : str):
-        await bot.load_extension(f"cogs.{cog.lower()}")
-
-    @bot.command()
-    async def reload(ctx : commands.context, cog : str):
-        await bot.reload_extension(f"cogs.{cog.lower()}")
-
-    @bot.command()
-    async def unload(ctx : commands.context, cog : str):
-        await bot.unload_extension(f"cogs.{cog.lower()}")
+    async def ping(ctx : commands.Context):
+        embed = discord.Embed(colour=discord.Colour.green(), title="PONG")
+        ctx.send(embed=embed)
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
